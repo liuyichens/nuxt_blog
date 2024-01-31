96487820e15330eaeb32855e2b049a07bad68b1f
liuyichen <2643150172@qq.com>
2024-01-31 08:30:32 +0800

    增加一些演示文本

--- components/content/ProseCode.vue
+++ components/content/ProseCode.vue
@@ -1,20 +1,52 @@
-<script setup lang="ts">
+<script lang="ts" setup>
 defineProps<{
-  code: string,
+  code: string
+  language?: string
   filename?: string
+  meta?: string
 }>()
+import {useClipboard} from "@vueuse/core";
+
+const { copy, copied, isSupported } = useClipboard()
 </script>
 
 <template>
-<div class="code-block relative [&>pre]:!rounded-t-none [&>pre]:!my-0 my-5">
-<!--  <div class="code-block-head">-->
-<!--    FileName-->
-<!--  </div>-->
-<!--  <div>Copy Button</div>-->
-  <slot/>
-</div>
+  <div class="my-8 ring-1 ring-gray-300 dark:ring-gray-700 rounded-lg overflow-hidden">
+    <div class="group relative p-4 bg-gray-50 dark:bg-gray-700/20">
+      <span v-if="filename" class="absolute top-2 right-3 text-xs font-mono">
+        {{ filename }}
+      </span>
+      <slot />
+      <ClientOnly>
+        <button
+            v-if="isSupported" class="absolute right-3 bottom-2 p-1 opacity-0 group-hover:opacity-100 transition ease-in"
+            @click="copy(code)"
+        >
+          <span class="sr-only">
+            Copy to clipboard
+          </span>
+          <Transition
+              appear
+              mode="out-in"
+              enter-active-class="duration-150 ease-int"
+              enter-from-class="opacity-0"
+              enter-to-class="opacity-100"
+              leave-active-class="duration-150 ease-out"
+              leave-from-class="opacity-100"
+              leave-to-class="opacity-0"
+          >
+            <div v-if="copied" class="i-heroicons-check-solid w-4 h-4" />
+            <div v-else class="i-heroicons-clipboard-solid w-4 h-4" />
+          </transition>
+        </button>
+      </ClientOnly>
+    </div>
+  </div>
 </template>
 
 <style scoped>
-
+:deep(pre code .line) {
+  display: block;
+  min-height: 1rem;
+}
 </style>
